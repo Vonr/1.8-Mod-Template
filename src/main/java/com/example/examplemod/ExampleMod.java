3f917200b26aa350b4c2b89fdf3c5c6759d475c3
Qther <dtaclanlegion@gmail.com>
2021-03-07 14:51:29 +0800

    Semver and mod name

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -7,11 +7,17 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION , acceptableRemoteVersions = "*")
+@Mod(
+        modid = ExampleMod.MODID,
+        name = ExampleMod.MOD_NAME,
+        version = ExampleMod.VERSION ,
+        acceptableRemoteVersions = "*"
+)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MOD_ID = "examplemod";
+    public static final String MOD_NAME = "ExampleMod";
+    public static final String VERSION = "1.0.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
